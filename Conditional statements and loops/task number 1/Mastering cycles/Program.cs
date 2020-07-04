c05acd0f31a167763c7d7f5c11c77f67772747dc
Onixfff <60624718+Onixfff@users.noreply.github.com>
2020-07-04 15:15:35 +0200

    added Conditional statements and loops and added task Mastering cycles

---
+++ Conditional statements and loops/task number 1/Mastering cycles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastering_cycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            Console.WriteLine("Запишите предложение:");
+            int number;
+            text = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Сколько раз повторить этот текст");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number;  i++)
+            {
+                Console.WriteLine(text);
+            }
+            Console.ReadKey();
+        }
+    }
+}
