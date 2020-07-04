3bb2b916e592c7ffc63f5eb245897275ad6b8ee9
Onixfff <60624718+Onixfff@users.noreply.github.com>
2020-07-04 13:18:49 +0200

    added Basick of programming number 3

---
+++ Basick of programming number 3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics_of_programming_number_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ряд = 3;
+            int картинки = 52;
+            Console.WriteLine("Сколько рядов хотите вывести?");
+            int скролькоРяд = Convert.ToInt32(Console.ReadLine());
+            int t = ряд * скролькоРяд;
+            int НеНадоКартинок = картинки - t;
+            Console.WriteLine($"Выведется {t} картинок и {НеНадоКартинок} не появится на экране");
+            Console.ReadKey();
+        }
+    }
+}
