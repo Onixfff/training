74987d9ea0c9ca06fbfa5dfa218f88e844f4e63f
Onixfff <60624718+Onixfff@users.noreply.github.com>
2020-07-04 12:13:29 +0200

    added basics of programming number 3

---
+++ Basics of programming number 3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics_of_programming_number_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int crystals;
+            int gold;
+            string choice;
+            Console.WriteLine("Введите какое кол-во gold у вас есть:");
+            gold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("купить crystals по цене 2 gold = 1 crystals");
+            Console.ReadKey();
+            crystals = gold / 2;
+            Console.WriteLine($"Вы смогли купить {crystals} crystals за ваши {gold} gold");
+            Console.ReadKey();
+        }
+    }
+}
