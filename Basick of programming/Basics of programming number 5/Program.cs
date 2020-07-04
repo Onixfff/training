2f4a605149b5e8ff2bae2289d728b3c2b44ad84c
Onixfff <60624718+Onixfff@users.noreply.github.com>
2020-07-04 22:28:57 +0200

    Error correction

--- Basick of programming/Basics of programming number 5/Program.cs
+++ Basick of programming/Basics of programming number 5/Program.cs
@@ -11,17 +11,16 @@ namespace Basics_of_programming_number_5
         static void Main(string[] args)
         {
             int queue;
-            int time = 10;
-            int wait;
+            int waitingTime1Person = 10;
+            int waitTime;
             int hout;
             int min;
             Console.WriteLine("Сколько в очереди человек: ");
             queue = Convert.ToInt32(Console.ReadLine());
-            wait = queue * time;
-            hout = wait / 60;
-            min =  queue * 10 % 60;
+            waitTime = queue * waitingTime1Person;
+            hout = waitTime / 60;//Время ожидания в часах. для вывода
+            min =  queue * waitingTime1Person % 60;//Время ожидания в мин. для вывода
             Console.WriteLine($"Вы должны отстоять в очереди {hout} часов и {min} минут.");
-            //Console.WriteLine($"Вы должны отстоять в очереди {wait} часа и {wait} минут.");
             Console.ReadKey();
         }
     }
