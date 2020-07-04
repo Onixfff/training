b491d664520d90f1560da6c946040d0ae3b5c003
Onixfff <60624718+Onixfff@users.noreply.github.com>
2020-07-04 14:35:57 +0200

    added Basick of programming number 5

---
+++ Basics of programming number 5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics_of_programming_number_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int queue;
+            int time = 10;
+            int wait;
+            int hout;
+            int min;
+            Console.WriteLine("Сколько в очереди человек: ");
+            queue = Convert.ToInt32(Console.ReadLine());
+            wait = queue * time;
+            hout = wait / 60;
+            min =  queue * 10 % 60; 
+            Console.WriteLine($"Вы должны отстоять в очереди {hout} часов и {min} минут.");
+            Console.ReadKey();
+        }
+    }
+}
