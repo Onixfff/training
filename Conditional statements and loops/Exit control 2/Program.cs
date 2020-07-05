dcb095c72ae55594138cd8b320cd57f5d8731bc0
Onixfff <60624718+Onixfff@users.noreply.github.com>
2020-07-05 22:35:59 +0200

    added exit control

---
+++ Conditional statements and loops/Exit control 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exit_control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exit = "";
+            do
+            {
+                Console.WriteLine("Введите слово exit");
+                exit = Console.ReadLine();
+            } while (exit != "exit");
+
+            Console.WriteLine("Цикл закончился");
+            Console.ReadKey();
+        }
+    }
+}
