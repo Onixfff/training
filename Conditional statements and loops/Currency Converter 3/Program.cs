86eaea931cec5b0af012b93d5b5372527dcb3a79
Daniil Nikonov <danilnikonov01@mail.ru>
2020-07-07 01:32:07 +0200

    delite launch.json

---
+++ Conditional statements and loops/Currency Converter 3/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics_of_programming_number_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
