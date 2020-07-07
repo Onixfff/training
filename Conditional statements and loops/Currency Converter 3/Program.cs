c813ba2d338815142533708eefd13ac2e92aac15
Daniil Nikonov <danilnikonov01@mail.ru>
2020-07-07 14:58:32 +0200

    added Currency Converter

--- Conditional statements and loops/Currency Converter 3/Program.cs
+++ Conditional statements and loops/Currency Converter 3/Program.cs
@@ -10,7 +10,28 @@ namespace Basics_of_programming_number_6
     {
         static void Main(string[] args)
         {
+            float rub;
+            float usd;
+            float eur;
+            string userTnput;
+            float currencyCount;
+
+            rub = Convert.ToSingle(Console.ReadLine());
+            usd = Convert.ToSingle(Console.ReadLine());
+            eur = Convert.ToSingle(Console.ReadLine());
             
+            Console.WriteLine("Добро пожаловать в нашь обменник." +
+            +" Здесь вы сможете обменять свои рубли на долары, рубли на евро,"
+            +" долары на рубли, долары на евро, евро на рубли, евро на долары.");
+            Console.WriteLine("1-Обменять рубли");
+            Console.WriteLine("2-Обменять долары");
+            Console.WriteLine("3-Обменять евро");
+
+            switch(userTnput)
+            {
+                case "1" 
+                
+            }
         }
     }
 }
