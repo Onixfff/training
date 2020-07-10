b2c5a2ef01cf397776fed84d933d35d11effe9bb
Onixfff <60624718+Onixfff@users.noreply.github.com>
2020-07-11 00:25:35 +0200

    added new program (Program with password)

---
+++ Conditional statements and loops/Program with password/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_with_password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "password";
+            string userInput;
+
+            for (int i = 3; i > 0; i--)
+            {
+                Console.WriteLine($"Введите пороль.Попыток осталось {i}");
+                userInput = Console.ReadLine();
+                if (password == userInput)
+                {
+                    Console.WriteLine("Секретное сообщение!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Неправельно набранн пороль");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
