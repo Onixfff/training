ab4113a2eba3bfef117ff249da8c14de33073481
Onixfff <60624718+Onixfff@users.noreply.github.com>
2020-07-13 16:06:11 +0200

    added new task input Name

---
+++ Conditional statements and loops/input Name/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace input_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            char symbol;
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите символ");
+            symbol = Convert.ToChar(Console.ReadLine());
+
+            for (int i = -1; i <= name.Length; i++)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine();
+            Console.WriteLine(symbol + name + symbol);
+            for (int i = -1; i <= name.Length; i++)
+            {
+                Console.Write(symbol);
+            }
+            Console.ReadKey();
+        }
+    }
+}
