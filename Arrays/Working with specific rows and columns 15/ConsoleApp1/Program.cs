0f6ae9a21d33058e2bc30146fc51916673cfe592
Daniil Nikonov <danilnikonov01@mail.ru>
2020-07-23 17:49:50 +0200

    made a task on dz

--- Arrays/Working with specific rows and columns 15/ConsoleApp1/Program.cs
+++ Arrays/Working with specific rows and columns 15/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,27 @@ namespace ConsoleApp1
 {
     class Program
     {
-
+        static void Main(string[] args)
+        {
+            int[,] arr = {
+                {1,3,6},
+                {4,7,8},
+                {2,4,1}
+            };
+            for (int i = 0; i < arr.GetLength(0);i++)
+            {
+                for(int j = 0; j < arr.GetLength(1);j++)
+                {
+                    Console.Write(arr[i,j]+ " | ");
+                }
+                Console.WriteLine();
+            }
+            int sumLine, sumColumn;
+            sumLine = arr[1, 0] + arr[1, 1] + arr[1, 2];
+            sumColumn = arr[0, 0] * arr[1, 0] * arr[2, 0];
+            Console.WriteLine("Сумма 2 строки: "+sumLine);
+            Console.WriteLine("Произведение 1 столбца: "+sumColumn);
+            Console.ReadKey();
+        }
     }
 }
