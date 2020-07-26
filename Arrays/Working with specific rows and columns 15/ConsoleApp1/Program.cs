cef0340c62cca9c4ef56332f3171197883c11b7a
Daniil Nikonov <danilnikonov01@mail.ru>
2020-07-27 01:22:51 +0200

    changed the array to be self-contained

--- Arrays/Working with specific rows and columns 15/ConsoleApp1/Program.cs
+++ Arrays/Working with specific rows and columns 15/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,24 +11,33 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {   int sumLine, sumColumn;
+        {   int sumLine = 0, sumColumn = 1;
             int[,] arr = {
                 {1,3,6},
                 {4,7,8},
                 {2,4,1}
             };
             for (int i = 0; i < arr.GetLength(0);i++)
-            {
-                for(int j = 0; j < arr.GetLength(1);j++)
+            {   
+                if (i == 1)
                 {
+                   for (int j = 0; j < arr.GetLength(1); j++)
+                   {
+                        sumLine += arr[i,j];
+                   }
+                }
+                for(int j = 0; j < arr.GetLength(1);j++)
+                {   
+                    if (j == 0)
+                    {
+                        sumColumn *= arr[i,j];
+                    }
                     Console.Write(arr[i, j]+ " | ");
                 }
                 Console.WriteLine();
             }
-            sumLine = arr[1, 0] + arr[1, 1] + arr[1, 2];
-            sumColumn = arr[0, 0] * arr[1, 0] * arr[2, 0];
-            Console.WriteLine("Сумма 2 строки: "+sumLine);
-            Console.WriteLine("Произведение 1 столбца: "+sumColumn);
+            Console.WriteLine("Сумма 2 строки: " + sumLine);
+            Console.WriteLine("Произведение 1 столбца: " + sumColumn);
             Console.ReadKey();
         }
     }
