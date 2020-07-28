945fdc2dfc2a0dc42b07ee47cde6ccb6d2a0775c
Daniil Nikonov <danilnikonov01@mail.ru>
2020-07-28 18:47:07 +0200

    Решил 18 задачку

--- Arrays/Dynamic array 18/ConsoleApp1/Program.cs
+++ Arrays/Dynamic array 18/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,43 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {
+        {   int cycle = 0;
+            string UsingInputString;
+            int UsingInput, sum = 0;
+
+            while(true)
+            {
+                Console.WriteLine("Введите число");
+                UsingInput = Convert.ToInt32(Console.ReadLine());
+
+                int[] numbers = new int[cycle];
+                int[] newNumbers = new int [numbers.Length + 1];
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    newNumbers[i] = numbers[i];
+                }
+                newNumbers[newNumbers.Length - 1] = UsingInput;
+                numbers = newNumbers;
+
+                cycle++;
+
+                Console.WriteLine("Введите sum если надо подвести итог");
+                UsingInputString = Console.ReadLine();
+                
+                if (UsingInputString == "sum")
+                {
+                    for(int i = 0; i < numbers.Length; i++)
+                    {
+                        sum += numbers [i];
+                    }
+                    Console.WriteLine("Сумма: " + sum + "\nВведите любой символ для продолжения или exit для выхода");
+                    UsingInputString = Console.ReadLine();
+                }
+                if (UsingInputString == "exit")
+                {
+                    break;
+                }
+            }
         }
     }
 }
