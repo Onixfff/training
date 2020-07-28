ef743b2aa4e1283d1c95118965c890577515f4cd
Daniil Nikonov <danilnikonov01@mail.ru>
2020-07-28 21:07:33 +0200

    Сделал 17 задачу

--- Arrays/Local maxima 17/ConsoleApp1/Program.cs
+++ Arrays/Local maxima 17/ConsoleApp1/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.CSharp.RuntimeBinder;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int[] numbers = new int[30];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {   
+                numbers[i] = rnd.Next(10,100);
+            }
+
+            if(numbers[0] > numbers[1])
+            {
+                Console.WriteLine(numbers[0]);
+            }
+
+            for (int i = 1; i < numbers.Length - 1; i++)
+            {
+                if(numbers[i] > numbers [i + 1] && numbers[i] > numbers[i - 1])
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+
+            if(numbers[numbers.Length-1] > numbers[numbers.Length -2])
+            {
+                Console.WriteLine(+ numbers[numbers.Length - 1]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
