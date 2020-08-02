223db539f667e31d13a8fd184bb0939cc8960fcc
Onixfff <60624718+Onixfff@users.noreply.github.com>
2020-08-02 18:37:48 +0200

    Решаю 21 задачу.

--- Functions 19 - 23/ReadInt 21/ReadInt/Program.cs
+++ Functions 19 - 23/ReadInt 21/ReadInt/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,24 @@ namespace ReadInt
     {
         static void Main(string[] args)
         {
+            bool exit = true;
+            Console.Write("Введите число: ");
+            while (exit)
+            {
+                int number;
+                bool tryin = Int32.TryParse(Console.ReadLine(), out number);
+                if (tryin)
+                {
+                    Console.WriteLine("Всё верно ваше число - " + number);
+                    exit = false;
+                }
+                else
+                {
+                    Console.WriteLine("Error ваше число не может быть сконвертировано " + number);
+                }
+                Console.ReadKey();
+                Console.WriteLine("Введите число ещё раз");
+            }
         }
     }
 }
