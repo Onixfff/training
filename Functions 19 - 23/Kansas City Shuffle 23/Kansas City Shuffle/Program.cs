b76bd1f1aca347ab0c8ee9619b27b54209a5e134
Onixfff <60624718+Onixfff@users.noreply.github.com>
2020-08-06 23:12:48 +0200

    Начал 23 задачу

--- Functions 19 - 23/Kansas City Shuffle 23/Kansas City Shuffle/Program.cs
+++ Functions 19 - 23/Kansas City Shuffle 23/Kansas City Shuffle/Program.cs
@@ -10,6 +10,39 @@ namespace Kansas_City_Shuffle
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[10];
+            Filling(arr);
+            Output(arr);
+            Shuffle(arr);
+            Console.ReadKey();
+        }
+
+        static void Filling(int[]arr)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1, 10);
+            }
+        }
+
+        static void Output(int[] arr)
+        {
+            Console.Write("Начальный массив (");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0,2}", arr[i]);
+            }
+            Console.WriteLine(")");
+        }
+
+        static void Shuffle(int[]arr)
+        {
+            Console.Write("После перемешивания (");
+
+            
+
+            Console.Write(")");
         }
     }
 }
