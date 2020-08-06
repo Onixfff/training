86038e197ebf619a9462fb503d06b20faf308821
Onixfff <60624718+Onixfff@users.noreply.github.com>
2020-08-06 22:24:51 +0200

    Закончил 20 задачу

--- Functions 19 - 23/UIElement 20/UIElement/Program.cs
+++ Functions 19 - 23/UIElement 20/UIElement/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.DesignerServices;
 using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,46 +14,67 @@ namespace UIElement
         {
             while (true)
             {
-                int health = 2, maxHealth = 10;
-                int mana = 3, maxMana = 10;
-                Console.SetCursorPosition(0, 4);
-                Console.CursorVisible = true;
-                Console.Write("Введите число, на которое изменится жизни: ");
-                health += Convert.ToInt32(Console.ReadLine());
-                Console.Write("Введите число, на которое изменится мана: ");
-                mana += Convert.ToInt32(Console.ReadLine());
-
-                Console.CursorVisible = false;
-                drawBar(health, maxHealth, ConsoleColor.Red, 0);
-                drawBar(mana, maxMana, ConsoleColor.Blue, 1);
+                DrawBar(ConsoleColor.Red,0,"Введите сколько у вас жизней до 101 ");
+                ClearLine(4);
+                DrawBar(ConsoleColor.Blue,1,"Введите сколько у вас маны до 101 ");
+                ClearLine(4);
                 Console.ReadKey();
                 Console.Clear();
             }
         }
 
-        static void drawBar(int value, int maxValue, ConsoleColor color, int position, char symbol = '#')
+        static void DrawBar(ConsoleColor color, int position, string text, char symbol = '#')
         {
+            double defaultValue, value, maxValue = 10;
+
+            Console.SetCursorPosition(0, 4);
             ConsoleColor defaultColor = Console.BackgroundColor;
-            string bar = "";
-
-            for (int i = 0; i < value; i++)
+            Console.WriteLine(text);
+            value = Convert.ToSingle(Console.ReadLine());
+            defaultValue = value;
+            if (value <= 100)
             {
-                bar += symbol;
-            }
+                string bar = "";
+                value /= 10;
+                value = Math.Ceiling(value);
 
-            Console.SetCursorPosition(0, position);
-            Console.Write("[");
+                for (int i = 0; i < value; i++)
+                {
+                    bar += symbol;
+                }
+
+                Console.SetCursorPosition(0, position);
+                Console.Write("[");
+                Console.BackgroundColor = color;
+                Console.Write(bar);
+                Console.BackgroundColor = defaultColor;
+
+                bar = "";
+
+                for (double i = value; i < maxValue; i++)
+                {
+                    bar += " ";
+                }
+                Console.Write(bar + "] " + defaultValue + "%");
+
+            }
+            else
+                Error("Вы ввели неправельно проценты");
+        }
+
+        static void Error(string text = "Ошибочка !", ConsoleColor color = ConsoleColor.Red)
+        {
+            Console.SetCursorPosition(0, 0);
+            ConsoleColor defaultColor = Console.BackgroundColor;
             Console.BackgroundColor = color;
-            Console.Write(bar);
+            Console.Write(text);
             Console.BackgroundColor = defaultColor;
+        }
 
-            bar = "";
-
-            for (int i = value; i < maxValue; i++)
-            {
-                bar += " ";
-            }
-            Console.Write(bar + "]");
+        static void ClearLine(int line)
+        {
+            Console.MoveBufferArea(0, line , Console.BufferWidth,1,Console.BufferWidth, line, ' ', Console.ForegroundColor,Console.BackgroundColor);
+            Console.MoveBufferArea(0, line + 1, Console.BufferWidth, 1, Console.BufferWidth, line + 1, ' ', Console.ForegroundColor, Console.BackgroundColor);
         }
     }
 }
