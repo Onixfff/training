794b0d60e2e3854e98210bcd32c990d0f9c63b31
Daniil Nikonov <danilnikonov01@mail.ru>
2020-09-01 20:27:32 +0200

    Сделал 29 задачу

--- ООП 28 - 38/Working with properties №29/Working with properties №29/Program.cs
+++ ООП 28 - 38/Working with properties №29/Working with properties №29/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,33 @@ namespace Working_with_properties__29
     {
         static void Main(string[] args)
         {
+            Render rendered = new Render();
+            Player player = new Player(2,22);
+
+            rendered.DrawPlayer(player.X, player.Y);
+            Console.ReadKey();
         }
+    }
+
+    class Player
+    {
+        public int Y { get; private set; }
+
+        public int X { get; private set; }
+
+        public Player(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    class Render
+    {
+        public void DrawPlayer(int x, int y, char ch = '@')
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(ch);
+        }  
     }
 }
