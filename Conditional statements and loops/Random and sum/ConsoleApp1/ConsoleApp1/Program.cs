a231856e996d7d3c223446a1874b344cde98c2be
Данил Никонов <danilnikonov01@mail.ru>
2022-07-30 19:44:18 +0200

    Создан новый проект.

---
+++ Conditional statements and loops/Random and sum/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            
+            int randomNumber = random.Next(0,100);
+            int reducedNumber = randomNumber;
+            int sum = 0;
+
+            while (reducedNumber > 0)
+            {
+                if (reducedNumber % 3 == 0 || reducedNumber % 5 == 0)
+                {
+                    sum = sum + reducedNumber;
+                }
+                reducedNumber--;
+            }
+
+            Console.WriteLine($"{randomNumber}\n{sum}");
+            Console.ReadKey();
+        }
+    }
+}
