454df42edb182d32f530cd2ff5e0a870c6407bd7
Данил Никонов <danilnikonov01@mail.ru>
2022-07-30 19:03:28 +0200

    Удалил метод ShowInfo

--- Basick of programming/Basics of programming number 7/Basics of programming №7/Basics of programming №7.1/Program.cs
+++ Basick of programming/Basics of programming number 7/Basics of programming №7/Basics of programming №7.1/Program.cs
@@ -2,28 +2,22 @@
 
 namespace Basics_of_programming__7._1
 {
-    internal class Program
+    class Program
     {
-        public static string name = "Валантос";
-        public static string surname = "Danilka";
+
         static void Main(string[] args)
         {
-            OutPutInfo();
-            Swaps(ref name, ref surname);
-            OutPutInfo();
-            Console.ReadKey(true);
-        }
+            string name = "Антонин";
+            string surname = "Данилка";
 
-        public static void Swaps(ref string name, ref string surname)
-        {
+            Console.WriteLine($"Name - {name}\nSurname - {surname}\n");
+
             string variable = name;
             name = surname;
             surname = variable;
-        }
 
-        public static void OutPutInfo()
-        {
             Console.WriteLine($"Name - {name}\nSurname - {surname}\n");
+            Console.ReadKey(true);
         }
     }
 }
