211abc35de9a8c74375a390bb93d52396877093f
Данил Никонов <danilnikonov01@mail.ru>
2022-07-30 23:08:37 +0200

    Создал новый проект

---
+++ Conditional statements and loops/findingTheMinimumDegree/findingTheMinimumDegree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findingTheMinimumDegree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int statickNumber = 2;
+            int minRandom = 0;
+            int maxRandom = 100;
+            int randomNumber = random.Next(minRandom, maxRandom);
+            int sum = 0;
+
+            for(int degree = 0; sum < randomNumber; degree++)
+            {
+                sum = (int)Math.Pow(statickNumber, degree);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
