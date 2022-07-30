937b5deb1f1f169e06d35e468f2929abbd5bfa61
Данил Никонов <danilnikonov01@mail.ru>
2022-07-30 18:48:23 +0200

    Создал файл. Сделал 2 метода Swaps и OutPutInfo. В методе мейн создал 2 переменные и в программе поменял значения этих переменных местими с последующем выводом информации на консоль

---
+++ Basick of programming/Basics of programming number 7/Basics of programming №7/Basics of programming №7.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Basics_of_programming__7._1
+{
+    internal class Program
+    {
+        public static string name = "Валантос";
+        public static string surname = "Danilka";
+        static void Main(string[] args)
+        {
+            OutPutInfo();
+            Swaps(ref name, ref surname);
+            OutPutInfo();
+            Console.ReadKey(true);
+        }
+
+        public static void Swaps(ref string name, ref string surname)
+        {
+            string variable = name;
+            name = surname;
+            surname = variable;
+        }
+
+        public static void OutPutInfo()
+        {
+            Console.WriteLine($"Name - {name}\nSurname - {surname}\n");
+        }
+    }
+}
---
+++ Basick of programming/Basics of programming number 7/Basics of programming №7/Basics of programming №7/Program.cs
@@ -0,0 +1,4 @@
+﻿static void main()
+{
+    Console.WriteLine("www");
+}
