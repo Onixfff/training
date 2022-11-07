cc7dc850f08d1c3e9bdd65aebc3eef74d8fd9724
Данил Никонов <danilnikonov01@mail.ru>
2022-11-07 02:01:20 +0200

    Заменил 9 на переменную и дал ей имя максимальное натуральное число

--- Conditional statements and loops/multiples/Program.cs
+++ Conditional statements and loops/multiples/Program.cs
@@ -13,13 +13,14 @@ namespace multiples
             long result = 0;
             int min = 7;
             int max = 27;
+            int maxNaturalNumber = 9;
 
-            for(int firstDigit = 1; firstDigit < 9; ++firstDigit)
+            for (int firstDigit = 1; firstDigit < maxNaturalNumber; ++firstDigit)
             {
-                for(int secondDigit = 0; secondDigit < 9; ++secondDigit)
+                for(int secondDigit = 0; secondDigit < maxNaturalNumber; ++secondDigit)
                 {
                     int sumDemoResult = firstDigit + secondDigit;
-                    for(int thirdDigit = 0; thirdDigit < 9; ++thirdDigit)
+                    for(int thirdDigit = 0; thirdDigit < maxNaturalNumber; ++thirdDigit)
                     {
                         int sum = sumDemoResult + thirdDigit;
                         if (min <= sum && sum <= max)
