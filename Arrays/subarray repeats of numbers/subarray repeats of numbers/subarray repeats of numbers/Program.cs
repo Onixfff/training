e84e2f5f41247501ddd1b6ea5d40415460494175
Данил Никонов <danilnikonov01@mail.ru>
2022-12-14 11:15:29 +0200

    Удалил лишний пробел и изменил название переменных

--- Arrays/subarray repeats of numbers/subarray repeats of numbers/subarray repeats of numbers/Program.cs
+++ Arrays/subarray repeats of numbers/subarray repeats of numbers/subarray repeats of numbers/Program.cs
@@ -11,8 +11,8 @@ namespace subarray_repeats_of_numbers
         static void Main(string[] args)
         {
             int repeat = 0;
-            int f = 0;
-            int repeatg = 0;
+            int maxNumberRepeat = 0;
+            int maxRepeat = 0;
             int number = 0;
             int[] baseArray = new int[30] { 1,1,1,4,5,6,7,8,9,10,11,12,5,5,5,5,5,18,19,20,21,22,6,6,6,6,6,6,6,30 };
 
@@ -21,10 +21,10 @@ namespace subarray_repeats_of_numbers
                 Console.Write(baseArray[i]);
                 if (number != baseArray[i])
                 {
-                    if (repeat > repeatg)
+                    if (repeat > maxRepeat)
                     {
-                        f = number;
-                        repeatg = repeat;
+                        maxNumberRepeat = number;
+                        maxRepeat = repeat;
                     }
                     number = baseArray[i];
                     repeat = 1;
@@ -43,8 +43,7 @@ namespace subarray_repeats_of_numbers
                 }
             }
 
-            Console.Write($" - Число {f} повторяется подрят - {repeatg} раз ");
-
+            Console.Write($" - Число {maxNumberRepeat} повторяется подрят - {maxRepeat} раз ");
             Console.ReadKey();
         }
     }
