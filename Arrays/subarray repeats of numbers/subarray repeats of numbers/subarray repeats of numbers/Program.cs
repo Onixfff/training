8bfda67d9b1ef7b7c13182625bb4c07f7caedbbe
Данил Никонов <danilnikonov01@mail.ru>
2022-12-14 11:13:09 +0200

    Создал класс. В классе сделан массив вывод базового массива и проход по сассиву с нахождением максимально последовательно повторяющегося числа

---
+++ Arrays/subarray repeats of numbers/subarray repeats of numbers/subarray repeats of numbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subarray_repeats_of_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeat = 0;
+            int f = 0;
+            int repeatg = 0;
+            int number = 0;
+            int[] baseArray = new int[30] { 1,1,1,4,5,6,7,8,9,10,11,12,5,5,5,5,5,18,19,20,21,22,6,6,6,6,6,6,6,30 };
+
+            for(int i = 0; i < baseArray.Length; i++)
+            {
+                Console.Write(baseArray[i]);
+                if (number != baseArray[i])
+                {
+                    if (repeat > repeatg)
+                    {
+                        f = number;
+                        repeatg = repeat;
+                    }
+                    number = baseArray[i];
+                    repeat = 1;
+                }
+                else if (number == 0)
+                {
+                    number = baseArray[i];
+                    repeat = 1;
+                }
+                else
+                {
+                    if (number == baseArray[i])
+                    {
+                        repeat++;
+                    }
+                }
+            }
+
+            Console.Write($" - Число {f} повторяется подрят - {repeatg} раз ");
+
+            Console.ReadKey();
+        }
+    }
+}
