4ea8499978f9222ac4e1acfc75d95584a743760b
Данил Никонов <danilnikonov01@mail.ru>
2022-12-14 12:24:04 +0200

    Использовал метод split и вывел полученный массив, каждое слово с новой строки

---
+++ Arrays/Split/Split/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Коля дунул в универе";
+            string[] sentenceSplit = sentence.Split();
+
+            foreach (var item in sentenceSplit)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
