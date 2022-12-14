86315075923a336d477a3bafebf9006f46b9e3d3
Данил Никонов <danilnikonov01@mail.ru>
2022-12-14 12:15:07 +0200

    Создал класс сортировки массива методом вставки

---
+++ Arrays/sorting numbers/sorting numbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorting_numbers
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            int[] baseArray = new int[rnd.Next(10, 50)];
+
+            for (int i = 0; i < baseArray.Length; i++)
+            {
+                baseArray[i] = RandimizeNumber();
+                Console.Write(baseArray[i]);
+            }
+
+            for (int i = 1; i < baseArray.Length; i++)
+            {
+                int key = baseArray[i];
+                int j = i;
+                while ((j >= 1) && (baseArray[j - 1] > key))
+                {
+                    Swap(ref baseArray[j - 1], ref baseArray[j]);
+                    j--;
+                }
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in baseArray)
+            {
+                Console.Write(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Swap(ref int numberOne, ref int NumberTwo)
+        {
+            int temp = numberOne;
+            numberOne = NumberTwo;
+            NumberTwo = temp;
+        }
+
+        static int RandimizeNumber()
+        {
+            return rnd.Next(1, 10);
+        }
+    }
+}
