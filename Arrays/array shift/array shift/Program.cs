5efb26109d6bb727f124fb405ad060191dd42679
Данил Никонов <danilnikonov01@mail.ru>
2022-12-14 21:12:29 +0200

    Изменил цикл и исправил проход по циклу

--- Arrays/array shift/array shift/Program.cs
+++ Arrays/array shift/array shift/Program.cs
@@ -8,34 +8,27 @@ namespace array_shift
 {
     class Program
     {
-        static Random rnd = new Random();
         static void Main(string[] args)
         {
             int[] arrayNumbers = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             foreach (var item in arrayNumbers)
             {
-                Console.WriteLine(item);
+                Console.Write(item);
             }
 
             Console.Write("\nВведите сдвиг - ");
             int shift = Convert.ToInt32(Console.ReadLine());
-            int temp = 0;
-            int startNumberArray = 0;
 
-            while (temp != shift)
+            for(int j = shift; j > 0; j--)
             {
-                for(int i = 0; i < arrayNumbers.Length; i++)
+                int startNumberArray = arrayNumbers[0];
+
+                for (int i = 0; i < arrayNumbers.Length; i++)
                 {
-                    if (i == 0)
-                    {
-                        startNumberArray = arrayNumbers[i];
-                        arrayNumbers[i] = arrayNumbers[i + 1];
-                    }
-                    else if (i == arrayNumbers.Length - 1)
+                    if (i == arrayNumbers.Length - 1)
                     {
                         arrayNumbers[i] = startNumberArray;
-                        temp++;
                     }
                     else
                     {
