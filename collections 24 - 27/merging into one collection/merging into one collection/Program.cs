61fd05f2e25e77ab282f68d244ee8ca263be6655
Данил Никонов <danilnikonov01@mail.ru>
2022-12-14 17:03:31 +0200

    Создал класс который из 2 массивов string делает одну коллекцию без повторений

---
+++ collections 24 - 27/merging into one collection/merging into one collection/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace merging_into_one_collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] charsArrayTwo = new string[] { "2", "5", "6", "3" };
+            string[] charsArrayOne = new string[] { "1", "2", "3", "4" };
+            List<string> combinedCollection = new List<string>(charsArrayOne);
+
+            foreach (var item in combinedCollection)
+            {
+                Console.Write(item);
+            }
+
+            foreach (var item in charsArrayTwo)
+            {
+                bool check = combinedCollection.Contains(item);
+                if (check == false)
+                    combinedCollection.Add(item);
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in combinedCollection)
+            {
+                Console.Write(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
