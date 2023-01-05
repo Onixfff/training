2acb49494f6856ab90a6b33d96d93f16d6e2855b
Данил Никонов <danilnikonov01@mail.ru>
2023-01-05 12:13:26 +0200

    уцй

--- ООП 28 - 38/DeckOfCards/DeckOfCards/Program.cs
+++ ООП 28 - 38/DeckOfCards/DeckOfCards/Program.cs
@@ -6,10 +6,56 @@ using System.Threading.Tasks;
 
 namespace DeckOfCards
 {
+    enum Suit
+    {
+        Trump, //(Козырь)
+        Diamonds, //(Бубы / Алмазы)
+        Hearts, //(Черви / Сердца)
+        Clubs, //(Трефы / Клубы)
+        Spades, //(Пики / Лопаты)
+    }
+
+    enum Rank
+    {
+        Ace, // (Туз)
+        Jack, // (Валет / Джек)
+        Queen,// (Дама / Королева(
+        King,// (Король)
+        Joker,// (Джокер)
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine,
+        ten
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+
+        }
+    }
+
+    class Desk
+    {
+
+    }
+
+    class Card
+    {
+        public Suit suit { get; private set; }
+        public Rank rank { get; private set; }
+
+        public Card(Suit suit, Rank rank)
+        {
+            this.rank = rank;
+            this.suit = suit;
+
 
         }
     }
